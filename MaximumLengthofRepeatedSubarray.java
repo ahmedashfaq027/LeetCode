5a143ab3f9ca6a0aa0bab378767cd3f903c0bfe2
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2021-07-12 22:22:16 +0530

    Added description for MaximumLengthofRepeatedSubarray

--- MaximumLengthofRepeatedSubarray.java
+++ MaximumLengthofRepeatedSubarray.java
@@ -1,3 +1,22 @@
+/*
+
+https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+
+Given two integer arrays nums1 and nums2, return the maximum length of a subarray that appears in both arrays.
+
+Constraints:
+1. 1 <= nums1.length, nums2.length <= 1000
+2. 0 <= nums1[i], nums2[i] <= 100
+
+Input: nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7]
+Output: 3
+    Explanation: The repeated subarray with maximum length is [3,2,1].
+
+Input: nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+Output: 5
+
+*/
+
 import java.math.BigInteger;
 import java.util.*;
 
