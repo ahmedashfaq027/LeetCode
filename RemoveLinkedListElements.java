c8d7ef4be176fc1011b2a9227da1668316fbc3fa
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-12 21:40:24 +0530

    Solved RemoveLinkedListElements

--- RemoveLinkedListElements.java
+++ RemoveLinkedListElements.java
@@ -66,6 +66,23 @@ public class RemoveLinkedListElements {
     }
 
     public ListNode removeElements(ListNode head, int val) {
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+
+        ListNode temp = head;
+        while (temp != null && temp.next != null) {
+            if (temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+
+        return head;
+    }
+
+    public ListNode removeElementsComplex(ListNode head, int val) {
         if (head == null)
             return null;
 
