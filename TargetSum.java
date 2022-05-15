d88b3447bce990c9625c83fa27e3bc675898a189
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-15 17:32:03 +0530

    Solved TargetSum

--- TargetSum.java
+++ TargetSum.java
@@ -36,7 +36,45 @@ public class TargetSum {
         System.out.println(lc.findTargetSumWays(new int[]{1}, 1));
     }
 
+    /*
+        Explanation:
+            0/1 Knapsack problem
+    */
     public int findTargetSumWays(int[] nums, int target) {
+        int n = nums.length;
+
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+
+        if (Math.abs(target) > sum)
+            return 0;
+
+        if ((target + sum) % 2 != 0)
+            return 0;
+
+        int S = (sum + target) / 2;
+
+        int[][] dp = new int[n + 1][S + 1];
+        for (int i = 0; i < n + 1; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < S + 1; j++) {
+                if (nums[i - 1] <= j) {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][S];
+    }
+
+    public int findTargetSumWaysBackTrack(int[] nums, int target) {
         return findCount(nums, target, 0, 0);
     }
 
