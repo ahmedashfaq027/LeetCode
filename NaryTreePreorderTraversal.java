f0776e2848fb750c552a0f0fabfbda693bbdc562
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-16 23:41:38 +0530

    Solved NaryTreePreorderTraversal

--- NaryTreePreorderTraversal.java
+++ NaryTreePreorderTraversal.java
@@ -1,26 +1,31 @@
 /*
 
-https://leetcode.com/problems/binary-tree-inorder-traversal/
+https://leetcode.com/problems/n-ary-tree-preorder-traversal/
 
-Given the root of a binary tree, return the inorder traversal of its nodes' values.
+Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+
+Nary-Tree input serialization is represented in their level order traversal. Each group of children is separated by the null value (See examples)
 
 Constraints:
-1. The number of nodes in the tree is in the range [0, 100].
-2. -100 <= Node.val <= 100
+1. The number of nodes in the tree is in the range [0, 104].
+2. 0 <= Node.val <= 104
+3. The height of the n-ary tree is less than or equal to 1000.
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 
-Input: root = [1,null,2,3]
-Output: [1,3,2]
-    Explanation:           1
-                               2
-                           3
+Input: root = [1,null,3,2,4,null,5,6]
+Output: [1,3,5,6,2,4]
+    Explanation:            1
+                  3         2           4
+               5    6
 
-Input: root = []
-Output: []
-
-Input: root = [1]
-Output: [1]
+Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+    Explanation:                1
+                    2       3           4           5
+                          6   7         8           9
+                              11        12          13
+                              14
 
 */
 
