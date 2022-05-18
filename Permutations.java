756a0f3585c53215c689f7a2c8b5903813403deb
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-18 21:03:58 +0530

    Solved Permutations

--- Permutations.java
+++ Permutations.java
@@ -1,39 +1,63 @@
+/*
+
+https://leetcode.com/problems/permutations/
+
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Constraints:
+1. 1 <= nums.length <= 6
+2. -10 <= nums[i] <= 10
+3. All the integers of nums are unique.
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Input: nums = [1]
+Output: [[1]]
+
+*/
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
     public static void main(String[] args) {
         Permutations lc = new Permutations();
 
-        List<List<Integer>> res = lc.permute(new int[]{1, 2, 3});
-        for (List<Integer> i : res) {
-            System.out.println(Arrays.toString(i.toArray()));
-        }
+        System.out.println(lc.permute(new int[]{1, 2, 3}));
+        System.out.println(lc.permute(new int[]{0, 1}));
+        System.out.println(lc.permute(new int[]{1}));
     }
 
     public List<List<Integer>> permute(int[] nums) {
+        int n = nums.length;
+
         List<List<Integer>> res = new ArrayList<>();
-        permute(nums, new boolean[nums.length], new ArrayList<>(), res);
+        dfs(nums, new ArrayList<>(), res, new boolean[n]);
+
         return res;
     }
 
-    public void permute(int[] nums, boolean[] used, List<Integer> currPerm, List<List<Integer>> result) {
-        int n = nums.length;
-        if (currPerm.size() == n) {
-            result.add(new ArrayList<>(currPerm));
+    private void dfs(int[] nums, List<Integer> currPerm, List<List<Integer>> res, boolean[] visited) {
+        if (currPerm.size() == nums.length) {
+            res.add(new ArrayList<>(currPerm));
             return;
         }
 
-        for (int i = 0; i < n; i++) {
-            if (used[i])
+        for (int i = 0; i < nums.length; i++) {
+            if (visited[i])
                 continue;
 
-            used[i] = true;
             currPerm.add(nums[i]);
-            permute(nums, used, currPerm, result);
-            used[i] = false;
-            currPerm.remove(currPerm.size() - 1);
+            visited[i] = true;
+
+            dfs(nums, currPerm, res, visited);
+
+            currPerm.remove(Integer.valueOf(nums[i]));
+            visited[i] = false;
         }
     }
 }
