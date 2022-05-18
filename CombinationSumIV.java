813313f1dec6ed241c56df77090010f0e57a102c
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-18 21:46:45 +0530

    Solved CombinationsSumIV

--- CombinationSumIV.java
+++ CombinationSumIV.java
@@ -11,6 +11,8 @@ Constraints:
 3. All the elements of nums are unique.
 4. 1 <= target <= 1000
 
+Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
+
 Input: nums = [1,2,3], target = 4
 Output: 7
     Explanation:
@@ -27,8 +29,6 @@ Output: 7
 Input: nums = [9], target = 3
 Output: 0
 
-Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to allow negative numbers?
-
 */
 
 import java.util.Arrays;
@@ -42,6 +42,22 @@ public class CombinationSumIV {
     }
 
     public int combinationSum4(int[] nums, int target) {
+        int n = nums.length;
+        int[] dp = new int[target + 1];
+
+        dp[0] = 1;
+        for (int j = 1; j < target + 1; j++) {
+            for (int num : nums) {
+                if (j >= num) {
+                    dp[j] += dp[j - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+
+    public int combinationSum4Backtrack(int[] nums, int target) {
         int[] dp = new int[target + 1];
         Arrays.fill(dp, -1);
         return dfs(nums, target, dp);
