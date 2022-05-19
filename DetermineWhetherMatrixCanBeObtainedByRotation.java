3216d06eeb11d4c3872bcca5975edadb6f3fb6c2
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-19 19:27:21 +0530

    Solved DetermineWhetherMatrixCanBeObtainedByRotation

--- DetermineWhetherMatrixCanBeObtainedByRotation.java
+++ DetermineWhetherMatrixCanBeObtainedByRotation.java
@@ -1,3 +1,29 @@
+/*
+
+https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
+
+Given two n x n binary matrices mat and target, return true if it is possible to make mat equal to target by rotating mat in 90-degree increments, or false otherwise.
+
+Constraints:
+1. n == mat.length == target.length
+2. n == mat[i].length == target[i].length
+3. 1 <= n <= 10
+4. mat[i][j] and target[i][j] are either 0 or 1.
+
+Input: mat = [[0,1],[1,0]], target = [[1,0],[0,1]]
+Output: true
+    Explanation: We can rotate mat 90 degrees clockwise to make mat equal target.
+
+Input: mat = [[0,1],[1,1]], target = [[1,0],[0,1]]
+Output: false
+    Explanation: It is impossible to make mat equal to target by rotating mat.
+
+Input: mat = [[0,0,0],[0,1,0],[1,1,1]], target = [[1,1,1],[0,1,0],[0,0,0]]
+Output: true
+    Explanation: We can rotate mat 90 degrees clockwise two times to make mat equal target.
+
+*/
+
 import java.util.Arrays;
 
 public class DetermineWhetherMatrixCanBeObtainedByRotation {
