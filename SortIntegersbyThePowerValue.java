76a8615e8f0c29a1d11e6aa6fc02fcad73d44b22
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-21 01:00:55 +0530

    Solved SortIntegersbyThePowerValue

--- SortIntegersbyThePowerValue.java
+++ SortIntegersbyThePowerValue.java
@@ -46,21 +46,6 @@ class Pair {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Pair pair = (Pair) o;
-        return x == pair.x && y == pair.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-
-    @Override
     public String toString() {
         return "Pair{x=" + x + ", y=" + y + "}";
     }
