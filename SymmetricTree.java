728698f8ed654b8a7d3668f5c6a6d8c210dc80a5
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-21 17:55:15 +0530

    Solved SymmetricTree

--- SymmetricTree.java
+++ SymmetricTree.java
@@ -21,6 +21,22 @@ Output: false
 import java.util.LinkedList;
 import java.util.Queue;
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class SymmetricTree {
     public static void main(String[] args) {
         SymmetricTree lc = new SymmetricTree();
