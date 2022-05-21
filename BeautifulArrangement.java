24771779ae2b4bace4485c33d58f41531d5c76bd
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-21 16:18:16 +0530

    Solved BeautifulArrangement

--- BeautifulArrangement.java
+++ BeautifulArrangement.java
@@ -1,3 +1,31 @@
+/*
+
+https://leetcode.com/problems/beautiful-arrangement/
+
+Suppose you have n integers labeled 1 through n. A permutation of those n integers perm (1-indexed) is considered a beautiful arrangement if for every i (1 <= i <= n), either of the following is true:
+1. perm[i] is divisible by i.
+2. i is divisible by perm[i].
+
+Given an integer n, return the number of the beautiful arrangements that you can construct.
+
+Constraints:
+1. 1 <= n <= 15
+
+Input: n = 2
+Output: 2
+    Explanation:
+    The first beautiful arrangement is [1,2]:
+        - perm[1] = 1 is divisible by i = 1
+        - perm[2] = 2 is divisible by i = 2
+    The second beautiful arrangement is [2,1]:
+        - perm[1] = 2 is divisible by i = 1
+        - i = 2 is divisible by perm[2] = 1
+
+Input: n = 1
+Output: 1
+
+*/
+
 public class BeautifulArrangement {
     public static void main(String[] args) {
         BeautifulArrangement lc = new BeautifulArrangement();
