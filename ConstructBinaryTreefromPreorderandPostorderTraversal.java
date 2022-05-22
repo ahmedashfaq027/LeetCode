fb88f82fba8c704aaf4661bdbb7223605427a7be
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-22 18:15:30 +0530

    Solved ConstructBinaryTreefromPreorderandPostorderTraversal

--- ConstructBinaryTreefromPreorderandPostorderTraversal.java
+++ ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -22,6 +22,22 @@ Output: [1]
 
 */
 
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
 public class ConstructBinaryTreefromPreorderandPostorderTraversal {
     public static void main(String[] args) {
         ConstructBinaryTreefromPreorderandPostorderTraversal lc = new ConstructBinaryTreefromPreorderandPostorderTraversal();
