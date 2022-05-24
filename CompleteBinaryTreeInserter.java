36d6c8864765247f6b76369d6391c6d6a0f6a4c8
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2022-05-24 17:32:10 +0530

    Solved CompleteBinaryTreeInserter

--- CompleteBinaryTreeInserter.java
+++ CompleteBinaryTreeInserter.java
@@ -1,5 +1,54 @@
+/*
+
+https://leetcode.com/problems/complete-binary-tree-inserter/
+
+A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.
+Design an algorithm to insert a new node to a complete binary tree keeping it complete after the insertion.
+
+Implement the CBTInserter class:
+1. CBTInserter(TreeNode root) Initializes the data structure with the root of the complete binary tree.
+2. int insert(int v) Inserts a TreeNode into the tree with value Node.val == val so that the tree remains complete, and returns the value of the parent of the inserted TreeNode.
+3. TreeNode get_root() Returns the root node of the tree.
+
+Constraints:
+1. The number of nodes in the tree will be in the range [1, 1000].
+2. 0 <= Node.val <= 5000
+3. root is a complete binary tree.
+4. 0 <= val <= 5000
+5. At most 104 calls will be made to insert and get_root.
+
+Input
+    ["CBTInserter", "insert", "insert", "get_root"]
+    [[[1, 2]], [3], [4], []]
+Output
+    [null, 1, 2, [1, 2, 3, 4]]
+
+    Explanation
+        CBTInserter cBTInserter = new CBTInserter([1, 2]);
+        cBTInserter.insert(3);  // return 1
+        cBTInserter.insert(4);  // return 2
+        cBTInserter.get_root(); // return [1, 2, 3, 4]
+
+*/
+
 import java.util.LinkedList;
 import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
 
 class CBTInserter {
     Queue<TreeNode> q;
@@ -48,22 +97,6 @@ class CBTInserter {
 
     public TreeNode get_root() {
         return root;
-    }
-}
-
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
     }
 }
 
